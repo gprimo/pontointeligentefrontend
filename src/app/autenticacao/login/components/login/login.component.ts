2a7798f615b60995954f60e599ecce0eab6091b0
gprimo <glaucoprimo@gmail.com>
2021-02-28 22:56:09 -0300

    autenticação funcionando com a api de verdade

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { Login } from '../../models';
+import { LoginService } from '../../services';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login-pf',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder, 
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private loginService: LoginService) { }
+
+  ngOnInit() {
+    this.gerarForm();
   }
 
+  gerarForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  logar() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const login: Login = this.form.value;
+    this.loginService.logar(login)
+      .subscribe(
+        (        data: { [x: string]: { [x: string]: string; }; }) => {
+          console.log(JSON.stringify(data));
+          localStorage['token'] = data['data']['token'];
+          const usuarioData = JSON.parse(
+            atob(data['data']['token'].split('.')[1]));
+          console.log(JSON.stringify(usuarioData));
+          if (usuarioData['role'] == 'ROLE_ADMIN') {
+          	alert('Deve redirecionar para a página de admin');
+            //this.router.navigate(['/admin']);
+          } else {
+          	alert('Deve redirecionar para a página de funcionário');
+            //this.router.navigate(['/funcionario']);
+          }
+        },
+        (        err: { [x: string]: number; }) => {
+          console.log(JSON.stringify(err));
+          let msg: string = "Tente novamente em instantes.";
+          if (err['status'] == 401) {
+            msg = "Email/senha inválido(s)."
+          }
+          this.snackBar.open(msg, "Erro", { duration: 5000 });
+        }
+      );
+  }
 }
+
+
+
+
+
+
+
